fea249ef10efba7a1ab0098d3199018df5f38be4
florapetersen <floramarina@comcast.net>
2020-07-08 12:37:11 -0700

    initial commit

---
+++ spec/trail_finder_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe TrailFinder do
+  it "has a version number" do
+    expect(TrailFinder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
